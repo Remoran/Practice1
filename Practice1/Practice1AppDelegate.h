83e28464bf1d531378255395d7f3952ae5dc2f0b
Ryan <ryan@Ryan-Morans-Macbook-Pro.local>
2014-02-17 01:59:50 -0500

    Initial Commit

---
+++ Practice1/Practice1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Practice1AppDelegate.h
+//  Practice1
+//
+//  Created by Ryan on 2/17/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Practice1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Practice1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Practice1
+//
+//  Created by Ryan on 2/17/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Practice1AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Practice1AppDelegate class]));
+    }
+}
