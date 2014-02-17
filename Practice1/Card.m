77919fedb8cdbff534e0f0be783188fdde2142ef
Ryan <ryan@Ryan-Morans-Macbook-Pro.local>
2014-02-17 02:13:32 -0500

    “readme”

--- Practice1/Card.m
+++ Practice1/Card.m
@@ -2,8 +2,8 @@
 //  Card.m
 //  Practice1
 //
-//  Created by Michael Toth on 2/13/14.
-//  Copyright (c) 2014 Michael Toth. All rights reserved.
+//  Created by Ryan on 2/17/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
 //
 
 #import "Card.h"
---
+++ README.md
@@ -0,0 +1,2 @@
+Ended up copying a lot of code from sample in attempt to fix errors and ended up with:
+"Application windows are expected to have a root view controller at the end of application launch"
